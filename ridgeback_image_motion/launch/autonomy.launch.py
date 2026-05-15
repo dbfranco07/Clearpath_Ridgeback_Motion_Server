cb01030011783c3d1feb4bcebebdb7da4d918dfe
Deo Franco <159624216+dbfranco07@users.noreply.github.com>
2026-05-15 13:29:57 +0800

    fix: enable use_composition in launch arguments for autonomy.launch.py

--- ridgeback_image_motion/launch/autonomy.launch.py
+++ ridgeback_image_motion/launch/autonomy.launch.py
@@ -89,7 +89,7 @@ def generate_launch_description():
                 launch_arguments={
                     "namespace": "",
                     "use_sim_time": "false",
-                    "use_composition": "False",
+                    "use_composition": "True",
                     "autostart": "true",
                     "params_file": nav2_params,
                 }.items(),
